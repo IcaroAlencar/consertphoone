6c9aab1c681a57a29e9336d816d96e25b0cabbdc
@Icaro1030 <icaroalencar@live.com>
2021-04-03 15:22:53 -0400

    Main

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className = "Title">ConsertPhoone</h1>
-        <p className = "Subtitle">
-        Assistência 
-        Técnica de Celulares, Tablets e 
-        Computadores</p>
-        <div className = "card">
-          <p className="telefone">(65) 9 9952-0374 (65) 9 9649-6663</p>    
-          </div>
+        <div className="card">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className = "Title">ConsertPhoone</h1>
+          <p className = "Subtitle">
+          Assistência 
+          Técnica de Celulares, Tablets e 
+          Computadores</p>
+          <p className = "telefone1">(65) 9 9952-0374 (65) 9 9649-6663</p>
+        </div>
       </header>
     </div>
   );
