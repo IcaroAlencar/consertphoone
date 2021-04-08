d7121f9c7d123a9969b3d20c5fb7b4e7965dc2c0
@Icaro1030 <icaroalencar@live.com>
2021-04-08 19:21:17 -0400

    New Screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Header.css';
 import logo from '../Logo2.png';
 
+
 const Header = ({black}) => {
     return(
         <header className={black ? 'black' : 'header'}>
             <div className='header--logo'>
                 <img src={logo} alt='Logo ConsertPhoone'/>
             </div>
+
+            <button className={black ? 'orcamento' : 'orcamento--'}>Orçarmentos</button>
         </header>
     );
 }
